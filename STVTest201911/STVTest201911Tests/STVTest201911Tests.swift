b6653d1016ecb16e066f7c2d7403da7ac526c50b
IshibaSeiko <seiko.ishiba@st-ventures.jp>
2019-11-23 10:00:58 +0900

    Initial commit

---
+++ STVTest201911/STVTest201911Tests/STVTest201911Tests.swift
@@ -0,0 +1,34 @@
+//
+//  STVTest201911Tests.swift
+//  STVTest201911Tests
+//
+//  Created by STV-M025 on 2019/11/23.
+//  Copyright © 2019 STV-M025. All rights reserved.
+//
+
+import XCTest
+@testable import STVTest201911
+
+class STVTest201911Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
